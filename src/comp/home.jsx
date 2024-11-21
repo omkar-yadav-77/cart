0dc0e46bc9e335a5c04bd0fb8cab0ad10f131509
bhaveshdandavate <bhaveshdandavate2@gmail.com>
2024-11-21 15:44:51 +0530

    shopcart

---
+++ src/comp/home.jsx
@@ -0,0 +1,14 @@
+import { useEffect, usestate } from "react";
+import "/.home.css";
+import {Link} from "react-router-dom";
+import home_product from "/.home_product";
+import { AiFillEye, AiFillHeart, AiOutlineShoppingCart } from "react-icons/ai";
+import {
+    BiLogoFacebook,
+    BiLogoTwitter,
+    BiLogoInstagram,
+    BiLogoYoutube,
+  } from "react-icons/bi";
+  const home=({addtocart})=>{
+    
+  }
